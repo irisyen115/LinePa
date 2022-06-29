5a6626476b455e38f1fc776e55494bc976df5bfa
rbx <110b16890@mailst.cjcu.edu.tw>
2022-06-29 19:46:17 +0800

    add sqlite

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Music(models.Model):
+    song_name = models.TextField(default="song")
+    song_num = models.TextField(default="32410")
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = "songlist"
